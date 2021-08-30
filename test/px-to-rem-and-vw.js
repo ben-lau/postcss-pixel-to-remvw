6a33496eb7540694e5e0c3e9aa3419b08c46706b
Ben <liubaobin@inke.cn>
2021-08-30 10:16:58 +0800

    test: add test example

--- test/px-to-rem-and-vw.js
+++ test/px-to-rem-and-vw.js
@@ -25,4 +25,56 @@ describe('basic convert', () => {
 
     expect(processed).toBe(output);
   });
+
+  it('should work in basic example2', () => {
+    const input = `
+    h1 {
+      width: 20px;
+      height: 12vw;
+      font-size: 0.5rem;
+      font-size: 1px;
+    }`;
+    const output = `
+    h1 {
+      width: 0.26667rem;
+      width: 2.66667vw;
+      height: 12vw;
+      font-size: 0.5rem;
+      font-size: 0.01333rem;
+      font-size: 0.13333vw;
+    }`;
+    const processed = postcss(pxtorem()).process(input).css;
+
+    expect(processed).toBe(output);
+  });
+
+  it('should work in customize base size', () => {
+    const input = `
+    h1 {
+      margin: 0 0 20px 20px;
+      font-size: 32px;
+      line-height: 1.2;
+      letter-spacing: 1px;
+    }`;
+    const output = `
+    h1 {
+      margin: 0 0 0.1626rem 0.1626rem;
+      margin: 0 0 1.62602vw 1.62602vw;
+      font-size: 0.26016rem;
+      font-size: 2.60163vw;
+      line-height: 1.2;
+      letter-spacing: 0.00813rem;
+      letter-spacing: 0.0813vw;
+    }`;
+    const processed = postcss(
+      pxtorem({
+        baseSize: {
+          rem: 123,
+          vw: 12.3,
+        },
+      })
+    ).process(input).css;
+
+    expect(processed).toBe(output);
+  });
 });
